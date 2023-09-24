33e88ccdab2e785cdc879428c621dcc2ac3d1f29
gayusuryu <gayuksg1990@gmail.com>
2023-09-24 15:37:42 +0530

    authform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body/body.component';
+import { HeaderComponent } from './header/header/header.component';
+import { LoginComponent } from './login/login/login.component';
+import { BrowseComponent } from './browse/browse/browse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    LoginComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login/login.component.ts
@@ -0,0 +1,72 @@
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators,AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit{
+  isSignUp:boolean= false;
+  signInForm!:FormGroup;
+  signUpForm!:FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(){
+    this.signInForm = this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(8)]],
+    });
+
+    this.signUpForm = this.fb.group({
+      name:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      passwordGroup:this.fb.group({
+        password:['',[Validators.required,Validators.minLength(8)]],
+        confirmPassword:['',[Validators.required]]
+      },{
+        validator:this.passwordMatchValidator
+      })
+    
+    })
+  }
+
+//  passwordMatchValidator(form: FormGroup) {
+//     const password = form.get('password')?.value;
+//     const confirmPassword = form.get('confirmPassword')?.value;
+
+//     if (password !== confirmPassword) {
+//       form.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+//     } else {
+//       form.get('confirmPassword')?.setErrors(null);
+//     }
+//   }
+
+  passwordMatchValidator(c: AbstractControl): { [key: string]: boolean } | null {
+    const password = c.get('password');
+    const confirmPassword = c.get('confirmPassword');
+  
+    if (password?.pristine || confirmPassword?.pristine) {
+      return null;
+    }
+  
+    if (password?.value === confirmPassword?.value) {
+      return null;
+    }
+    return { match: true };
+  }
+
+
+  toggleSignInForm() {
+    this.isSignUp=!this.isSignUp;
+  }
+
+  submitSignIn(){
+
+  }
+
+  submitSignUp(){
+
+  }
+}
